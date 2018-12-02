9565563fccfd1930c9d8efa1afd7bb1c28967e4a
levi-vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2018-12-02 11:57:23 +0100

    day02-1: solution with tests

---
+++ day02/src/Day02-1.kt
@@ -0,0 +1,43 @@
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.stream.Collectors
+import kotlin.streams.toList
+
+fun main(vararg args: String) {
+    println(getBoxIdsChecksum(loadBoxIds()))
+}
+
+fun loadBoxIds(): List<String> {
+    return BufferedReader(FileReader("boxIds.txt"))
+            .lines()
+            .toList()
+}
+
+fun getBoxIdsChecksum(boxIds: List<String>): Long {
+    var twos: Long = 0
+    var threes: Long = 0
+
+    boxIds.stream()
+            .forEach { id ->
+                if(containsLetterThatIsPresentNTimes(id, 2)) twos++
+                if(containsLetterThatIsPresentNTimes(id, 3)) threes++
+            }
+
+    return twos * threes;
+}
+
+fun containsLetterThatIsPresentNTimes(string: String, times: Long): Boolean {
+    return getLetterCount(string).values
+            .stream()
+            .filter { count -> count == times }
+            .findAny().isPresent
+}
+
+fun getLetterCount(string: String): Map<String, Long> {
+    return string.split("").stream()
+            .filter { char -> !char.isEmpty() }
+            .collect(Collectors.groupingBy(
+                    { char -> char },
+                    Collectors.counting<String>()
+            ))
+}
---
+++ day02/test/Day02-1Test.kt
@@ -0,0 +1,29 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Day02_1Test {
+
+    @Test
+    fun test_getLetterCount() {
+        assertEquals(mapOf<String, Long>(Pair("a", 1), Pair("b", 1), Pair("c", 1), Pair("d", 1), Pair("e", 1), Pair("f", 1)), getLetterCount("abcdef"))
+        assertEquals(mapOf<String, Long>(Pair("a", 2), Pair("b", 3), Pair("c", 1)), getLetterCount("bababc"))
+        assertEquals(mapOf<String, Long>(Pair("a", 1), Pair("b", 2), Pair("c", 1), Pair("d", 1), Pair("e", 1)), getLetterCount("abbcde"))
+        assertEquals(mapOf<String, Long>(Pair("a", 1), Pair("b", 1), Pair("c", 3), Pair("d", 1)), getLetterCount("abcccd"))
+        assertEquals(mapOf<String, Long>(Pair("a", 2), Pair("b", 1), Pair("c", 1), Pair("d", 2)), getLetterCount("aabcdd"))
+        assertEquals(mapOf<String, Long>(Pair("a", 1), Pair("b", 1), Pair("c", 1), Pair("d", 1), Pair("e", 2)), getLetterCount("abcdee"))
+        assertEquals(mapOf<String, Long>(Pair("a", 3), Pair("b", 3)), getLetterCount("ababab"))
+    }
+
+    @Test
+    fun test_containsLetterThatIsPresentNTimes() {
+        assertEquals(true, containsLetterThatIsPresentNTimes("aab", 2))
+        assertEquals(true, containsLetterThatIsPresentNTimes("aabbb", 3))
+        assertEquals(false, containsLetterThatIsPresentNTimes("aabbb", 1))
+    }
+
+    @Test
+    fun test_getBoxIdsChecksum() {
+        assertEquals(12, getBoxIdsChecksum(listOf("abcdef", "bababc", "abbcde","abcccd","aabcdd","abcdee","ababab")))
+    }
+
+}
