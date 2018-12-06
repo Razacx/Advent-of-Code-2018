2a303d4c0ee35bf024a869e54ce7db717381b872
Razacx <levi.vandenbempt.sjcm@gmail.com>
2018-12-06 20:56:00 +0100

    day06: output formatting

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -16,12 +16,13 @@ fun main(vararg args: String) {
 
     val maxFiniteArea = finiteAreaIds.map { id -> grid.getAreaForCellsMatchingCondition { it == id } }.max()
 
-    println("Largest finite area: $maxFiniteArea")
+    println("Largest finite area: \n\t$maxFiniteArea")
+    println()
 
     // Part 2
     grid.fillWithCumulativeDistanceToAllCoordinates(coordinates)
     val areaWithCumulativeDistancesBelow10K = grid.getAreaForCellsMatchingCondition { it < 10000 }
 
-    println("Area with cumulative distances smaller than 10000: $areaWithCumulativeDistancesBelow10K")
+    println("Area with cumulative distances smaller than 10000: \n\t$areaWithCumulativeDistancesBelow10K")
 
 }
