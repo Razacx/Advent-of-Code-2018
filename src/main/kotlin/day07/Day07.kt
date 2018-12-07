c9c0b14b0658f4700889a42a7c34b571363a199e
Razacx <levi.vandenbempt.sjcm@gmail.com>
2018-12-07 20:50:28 +0100

    day07: solution for part 1 & 2 Refactoring needed for part 2 :p

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,8 +1,41 @@
 package day07
 
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.stream.Collectors
+
 data class Requirement(val id: String, val requirementFor: String)
 
-data class Node(val id: String, val requirements: MutableList<Node>)
+data class Node(val id: String, val requirements: MutableList<Node>, var executed: Boolean) {
+
+    fun findAvailableNodes(): List<Node> {
+        return if (!executed && requirements.all { it.executed }) {
+            listOf(this)
+        } else {
+            requirements
+                    .filter { !it.executed }
+                    .flatMap { it.findAvailableNodes() }
+                    .distinct()
+                    .sortedBy { it.id }
+        }
+    }
+
+    // This solving strategy is wrong :(
+    // Seems I didn't read the problem description well enough
+    fun executeNextRequirement(): String {
+        return if (requirements.none { !it.executed }) {
+            executed = true
+            id
+        } else {
+            requirements
+                    .filter { !it.executed }
+                    .sortedBy { it.id }
+                    .first()
+                    .executeNextRequirement()
+        }
+    }
+
+}
 
 fun parseRequirement(line: String): Requirement {
     val result = """Step ([A-Z]) must be finished before step ([A-Z]) can begin\.""".toRegex()
@@ -30,5 +63,101 @@ fun buildBaseNodes(requirements: List<Requirement>): List<Node> {
     return requirements
             .flatMap { listOf(it.id, it.requirementFor) }
             .distinct()
-            .map { Node(it, mutableListOf()) }
+            .map { Node(it, mutableListOf(), false) }
+}
+
+fun resolveGraphExecutionOrder(requirements: List<Requirement>): String {
+    val finalNode = buildDependencyGraph(requirements)
+    var executionOrder = ""
+
+    while (!finalNode.executed) {
+        val nextNode = finalNode.findAvailableNodes()
+                .sortedBy { it.id }
+                .first()
+        nextNode.executed = true
+        executionOrder += nextNode.id
+    }
+
+    return executionOrder
+}
+
+data class WorkOrder(var timeLeft: Int, val node: Node)
+
+//Brute forcing it...
+fun resolveGraphExecutionTime(requirements: List<Requirement>, workers: Int): Int {
+    val finalNode = buildDependencyGraph(requirements)
+    var timePassed = 0
+
+    val workerTasks: MutableMap<Int, WorkOrder?> = (0 until workers).map { it to null as WorkOrder? }.toMap().toMutableMap()
+
+    fun doWork() {
+        workerTasks.keys.forEach {
+            val workOrder = workerTasks[it]
+            if(workOrder != null) {
+                workOrder.timeLeft--
+                if(workOrder.timeLeft == 0) {
+                    workOrder.node.executed = true
+                    workerTasks[it] = null
+                }
+            }
+        }
+    }
+
+    fun assignAvailableWorkers(availableNodes: MutableList<Node>) {
+        val availableWorkers = workerTasks.keys.filter { workerTasks[it] == null }
+        for (worker in availableWorkers) {
+            if(availableNodes.isEmpty()) {
+                return
+            } else {
+                val node = availableNodes.removeAt(0)
+                workerTasks[worker] = WorkOrder(getNodeTimeRequirement(node), node)
+            }
+        }
+    }
+
+    while (!finalNode.executed) {
+        val availableNodes = finalNode.findAvailableNodes()
+        val notOccupiedNodes = availableNodes.filter { !workerTasks.values.map { it?.node }.contains(it) }
+        assignAvailableWorkers(notOccupiedNodes.toMutableList())
+
+        doWork()
+        timePassed++
+    }
+
+    return timePassed
+}
+
+fun getNodeTimeRequirement(node: Node): Int {
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(node.id) + 1 + 60
+}
+
+// This solving strategy is wrong :(
+// Seems I didn't read the problem description well enough
+//
+//fun resolveGraphExecutionOrder(requirements: List<Requirement>): String {
+//    val finalNode = buildDependencyGraph(requirements)
+//    var executionOrder = ""
+//
+//    while (!finalNode.executed) {
+//        executionOrder += finalNode.executeNextRequirement()
+//    }
+//
+//    return executionOrder
+//}
+
+fun main(vararg args: String) {
+
+    val requirements = BufferedReader(FileReader("input/day07/nodeRequirements.txt"))
+            .lines()
+            .map { parseRequirement(it) }
+            .collect(Collectors.toList())
+    val executionOrder = resolveGraphExecutionOrder(requirements)
+
+    println("Execution order: \n\t$executionOrder")
+    println()
+
+    val executionTime = resolveGraphExecutionTime(requirements, 5)
+
+    println("Execution time with 5 workers: \n\t${executionTime}s")
+
 }
--- src/test/kotlin/day07/Day07KtTest.kt
+++ src/test/kotlin/day07/Day07KtTest.kt
@@ -1,6 +1,7 @@
 package day07
 
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 
 class Day07KtTest {
@@ -22,7 +23,29 @@ class Day07KtTest {
                 Requirement("A", "B"),
                 Requirement("B", "C")
         )
-        assertEquals(Node("C", mutableListOf(Node("B", mutableListOf(Node("A", mutableListOf()))))), buildDependencyGraph(requirements))
+
+        val nodeA = Node("A", mutableListOf(), false)
+        val nodeB = Node("B", mutableListOf(nodeA), false)
+        val nodeC = Node("C", mutableListOf(nodeB), false)
+
+        assertEquals(nodeC, buildDependencyGraph(requirements))
+    }
+
+    @Test
+    fun test_buildDependencyGraph_Diamond() {
+        val requirements = listOf(
+                Requirement("A", "B"),
+                Requirement("A", "C"),
+                Requirement("B", "D"),
+                Requirement("C", "D")
+        )
+
+        val nodeA = Node("A", mutableListOf(), false)
+        val nodeB = Node("B", mutableListOf(nodeA), false)
+        val nodeC = Node("C", mutableListOf(nodeA), false)
+        val nodeD = Node("D", mutableListOf(nodeB, nodeC), false)
+
+        assertEquals(nodeD, buildDependencyGraph(requirements))
     }
 
     @Test
@@ -33,12 +56,53 @@ class Day07KtTest {
         )
         assertEquals(
                 listOf(
-                        Node("A", mutableListOf()),
-                        Node("B", mutableListOf()),
-                        Node("C", mutableListOf())
+                        Node("A", mutableListOf(), false),
+                        Node("B", mutableListOf(), false),
+                        Node("C", mutableListOf(), false)
                 ),
                 buildBaseNodes(requirements)
         )
+    }
+
+    @Test
+    fun test_node_executeNextRequirement() {
+        val nodeA = Node("A", mutableListOf(), false)
+        val nodeB = Node("B", mutableListOf(nodeA), false)
+        val nodeC = Node("C", mutableListOf(nodeA), false)
+        val nodeD = Node("D", mutableListOf(nodeC, nodeB), false)
+
+        assertEquals(nodeD.executeNextRequirement(), "A")
+        assertEquals(nodeD.executeNextRequirement(), "B")
+        assertEquals(nodeD.executeNextRequirement(), "C")
+        assertEquals(nodeD.executeNextRequirement(), "D")
+    }
+
+    @Test
+    fun test_resolveGraphExecutionOrder() {
+        val requirements = listOf(
+                Requirement("C", "A"),
+                Requirement("C", "F"),
+                Requirement("A", "B"),
+                Requirement("A", "D"),
+                Requirement("B", "E"),
+                Requirement("D", "E"),
+                Requirement("F", "E")
+        )
+        assertEquals("CABDFE", resolveGraphExecutionOrder(requirements))
+    }
+
+    @Test
+    fun test_resolveGraphExecutionTime() {
+        val requirements = listOf(
+                Requirement("C", "A"),
+                Requirement("C", "F"),
+                Requirement("A", "B"),
+                Requirement("A", "D"),
+                Requirement("B", "E"),
+                Requirement("D", "E"),
+                Requirement("F", "E")
+        )
+        assertEquals(15, resolveGraphExecutionTime(requirements, 2))
     }
 
 }
